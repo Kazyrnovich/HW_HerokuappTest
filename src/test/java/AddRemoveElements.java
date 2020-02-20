52bdb1c0a95c03abfcd817aa8ae305da9cad6879
Kaz <choco74@mail.ru>
2020-02-20 14:20:56 +0300

    Тест на добавление/удаление

--- src/test/java/AddRemoveElements.java
+++ src/test/java/AddRemoveElements.java
@@ -1,6 +1,10 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class AddRemoveElements {
     @Test
@@ -8,7 +12,14 @@ public class AddRemoveElements {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver browser = new ChromeDriver();
         browser.get("http://the-internet.herokuapp.com/add_remove_elements/");
+        browser.manage().window().maximize();
+        browser.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        WebElement element1 = browser.findElement(By.xpath("//*[@onclick='addElement()']"));
+        element1.click();
+        element1.click();
+        WebElement element2 = browser.findElement(By.xpath("//button[text()='Delete']"));
+        element2.click();
 
+        browser.quit();
     }
-
 }
